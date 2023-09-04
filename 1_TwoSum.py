18fadbb4e12893f2f500e44fb8f2e3a3ff15589f
Misu <s2348677@stud.uni-frankfurt.de>
2023-09-04 16:56:18 +0200

    Added binary search to the file. A more efficient solution might be possible using BinSearch.

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -5,3 +5,22 @@ def two_sum(nums: list[int], target: int) -> list[int]:
                 return [i, j]
             
 print(two_sum([2, 7, 11, 15], 26))
+
+
+# Implementing Binary Search for a more efficient solution 
+# to the Two Sum problem.
+
+def binary_search(arr: list[int], target: int) -> int:
+    left, right = 0, len(arr) - 1
+
+    while len <= right:
+        mid = left + (right - left) // 2 # Calculate the middle index
+
+        if arr[mid] == target:
+            return mid # Elem found, return its index
+        elif arr[mid] < target:
+            left = mid + 1 # Target is in the right half
+        else:
+            right = mid - 1 # Target is in the left half
+    
+    return -1 # Target not found in the array
