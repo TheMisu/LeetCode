0cdbe342a1b8e0e3c6c2e4cbdbc1705a8164cf34
Misu <s2348677@stud.uni-frankfurt.de>
2023-09-05 13:56:14 +0200

    Wrote a more efficient implementation for the Two Sum solution. The efficient implementation uses a loop of len n and Binary Search (O(log n)) to find the pair that adds up to the target. This results in a total runtime of O(n log n).

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -13,7 +13,7 @@ print(two_sum([2, 7, 11, 15], 26))
 def binary_search(arr: list[int], target: int) -> int:
     left, right = 0, len(arr) - 1
 
-    while len <= right:
+    while left <= right:
         mid = left + (right - left) // 2 # Calculate the middle index
 
         if arr[mid] == target:
@@ -24,3 +24,11 @@ def binary_search(arr: list[int], target: int) -> int:
             right = mid - 1 # Target is in the left half
     
     return -1 # Target not found in the array
+
+def efficient_two_sum(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums) - 1):
+        pair = binary_search(nums, target - nums[i])
+        if pair != -1:
+            return [i, pair]
+
+print(efficient_two_sum([2, 7, 11, 15], 26))
