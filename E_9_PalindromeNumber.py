3441504ca8b2395202be372abbbd4958cb58e984
Misu <s2348677@stud.uni-frankfurt.de>
2023-09-06 14:02:33 +0200

    Finished writing the solution to problem 9.

---
+++ E_9_PalindromeNumber.py
@@ -0,0 +1,11 @@
+def is_palindrome(x: int) -> bool:
+    xStr = str(x)
+
+    isPalin = True
+    for i in range(len(xStr)):
+        if xStr[i] != xStr[(len(xStr) - 1) - i]:
+            isPalin = False
+    
+    return isPalin
+
+print(is_palindrome(121))
